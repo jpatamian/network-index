c090f0ef3420c2bf2b54bb21de9316200be5faf0
jpatamian <jpatamian@gmail.com>
2026-02-17 18:47:56 -0500

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import RubyPlugin from "vite-plugin-ruby"
 
 export default defineConfig({
-  plugins: [react()],
-  root: './app/frontend',
+  plugins: [
+    RubyPlugin(),
+    react(),
+  ],
+  // Optional: if you want the @ alias
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './app/frontend'),
+      "@": "/app/frontend",
     },
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
-  },
-  build: {
-    outDir: path.resolve(__dirname, './public/vite'),
-    emptyOutDir: true,
   },
 })
