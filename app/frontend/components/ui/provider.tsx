8d524215ef09439e3c5286ff3336466ab4e67317
jpatamian <jpatamian@gmail.com>
2026-02-17 20:23:46 -0500

    add base chakra

---
+++ app/frontend/components/ui/provider.tsx
@@ -0,0 +1,21 @@
+// AppProvider wraps the application with Chakra UI and theme support
+// Chakra UI provides component styling system and theming
+// next-themes enables light/dark mode switching
+import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react"
+import { ThemeProvider } from "next-themes"
+
+interface AppProviderProps {
+  children: React.ReactNode
+}
+
+const system = createSystem(defaultConfig)
+
+export function AppProvider({ children }: AppProviderProps) {
+  return (
+    <ChakraProvider value={system}>
+      <ThemeProvider attribute="class" disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+    </ChakraProvider>
+  )
+}
